c6fb3dcea5e761d8352436cfaa4517cc63053e3d
Mohamed-Eid01 <mohamedaid8331@gmail.com>
2025-12-15 17:23:47 +0200

    refactor

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -5,7 +5,7 @@ import ArrowScroll from "../comonents/ArrowScroll";
 
 function CategoryPage() {
   const { category } = useParams();
-  const products = useSelector((state) => state.product.products); 
+  const products = useSelector((state) => state.product.products);
 
   const filteredProducts = products.filter(
     (product) => product.category.toLowerCase() === category.toLowerCase()
@@ -13,7 +13,8 @@ function CategoryPage() {
 
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6">{category} Products</h1>
+      <h1 className="text-3xl font-bold mb-6">Category:</h1>
+      <h3 className="text-2xl font-bold mb-6">{category} Products</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
         {filteredProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
